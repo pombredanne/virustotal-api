147ab4c73c829fbf815365ce333473eefe145573
blacktop <blacktop@users.noreply.github.com>
2014-05-18 19:48:56 -0600

    Fix Build

--- virus_total_apis/__init__.py
+++ virus_total_apis/__init__.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 
 __title__ = 'virustotal-api'
-__version__ = '1.0.4'
+__version__ = '1.0.5'
 __author__ = 'Josh Maine'
 __license__ = 'GPLv3'
 __copyright__ = 'Copyright (C) 2014 Josh "blacktop" Maine'
