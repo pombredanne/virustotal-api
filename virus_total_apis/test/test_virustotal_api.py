a22fc55021201601ddf1319db78d15bfa3c0db4f
blacktop <blacktop@users.noreply.github.com>
2016-03-13 21:30:26 -0600

    fix exe path

--- virus_total_apis/test/test_virustotal_api.py
+++ virus_total_apis/test/test_virustotal_api.py
@@ -94,7 +94,7 @@ class InitTests(TestCase):
         vt = PublicApi(API_KEY)
 
         try:
-            print(json.dumps(vt.scan_file('test.exe'), sort_keys=False, indent=4))
+            print(json.dumps(vt.scan_file('test/test.exe), sort_keys=False, indent=4))
         except Exception as e:
             self.fail(e)
 
